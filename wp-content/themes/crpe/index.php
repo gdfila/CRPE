6db1fb0d557e1b9475dba35104614ea9871a7039
kevin <kevinlay.fr@gmail.com>
2015-04-06 16:57:31 +0200

    commentaire html index.php

--- wp-content/themes/crpe/index.php
+++ wp-content/themes/crpe/index.php
@@ -11,7 +11,7 @@ Récuperation du header
      -->            
     <div class="container_slider">
         <?php echo do_shortcode('[image-carousel showcaption="true"]'); ?>
-    </div>
+    </div><!-- /container_slider -->
 
     <!--
     Section pour mettre en avant le texte, exemple : l'accroche. Un h1 pour le titre général de la page et un texte grossi
@@ -21,7 +21,7 @@ Récuperation du header
             <h1 class="h1_tall">Preparez le concours de recrutement de professeur des écoles</h1>
             <h2 class="h2_medium">Rejoindre le Cours Galien, c’est la garantie d’une préparation sérieuse et adaptée, vous permettant d’aborder en toute confiance le Concours de Recrutement de Professeur des Ecoles.</h2>
         </div><!-- /container -->
-    </section>  
+    </section><!-- /accroche -->
 
     <!--
     Section présentant les valeurs de l'école
@@ -33,28 +33,28 @@ Récuperation du header
             <div class="row">
                 <h1 class="h1_tall text_shadow">Adoptez l'esprit Galien !</h1>
                 <div class="col-sm-3">
-                    <div class=" cercle-3">
+                    <div class="cercle-3">
                         <h2 class="h2_small"><strong>Partage</strong></h2>
                         <p>Nos professeurs spécialistes partagent leurs connaissances et vous donnent leurs astuces pour réussir le jour J.</p>
-                    </div>
+                    </div><!-- /cercle-3 -->
                 </div><!-- /col-sm-3 -->
                 <div class="col-sm-3">
                     <div class="cercle-3">
                         <h2 class="h2_small"><strong>Progression</strong></h2>
                         <p>Elle repose sur trois étapes essentielles qui mènent vers le succès : entraînement, méthode et encadrement.</p>
-                    </div>
+                    </div><!-- /cercle-3 -->
                 </div><!-- /col-sm-3 -->
                 <div class="col-sm-3">
                     <div class="cercle-3">
                         <h2 class="h2_small"><strong>Cohésion</strong></h2>
                         <p>Il ne peut y avoir de partage sans cohésion. Toutes nos équipes sont focalisées sur un seul objectif : votre réussite.</p>
-                    </div>
+                    </div><!-- /cercle-3 -->
                 </div><!-- /col-sm-3 -->
                 <div class="col-sm-3">
                     <div class="cercle-3">
                         <h2 class="h2_small"><strong>Réussite</strong></h2>
                         <p>Ultime étape des efforts fournis et pour laquelle nous vous accompagnons chaque jour grâce à une stratégie de concours précise.</p>
-                    </div>
+                    </div><!-- /cercle-3 -->
                 </div><!-- /col-sm-3 -->
             </div><!-- /row -->
         </div><!-- /container -->
@@ -67,8 +67,7 @@ Récuperation du header
         <div class="container">
             <h1 class="h1_medium">Ils ont choisi Galien</h1>
             <div class="row">
-                  <div id="carousel-example-generic" class="temoignage_slider slide" data-ride="carousel">
-
+                <div id="carousel-example-generic" class="temoignage_slider slide" data-ride="carousel">
                     <!-- Wrapper for slides -->
                     <div class="carousel-inner temoignage_slider_inner">
                         <div class="item active">
@@ -76,42 +75,42 @@ Récuperation du header
                                 <div class="embed-responsive embed-responsive-16by9 col-sm-9">
                                     <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/rk93EIqVd8g"></iframe>
                                 </div>
-                            </div>
+                            </div><!-- /col-sm-6 -->
                             <div class="col-sm-5 temoignage_back">
-                              <div class="temoignage_slider-caption">
+                              <div class="temoignage_slider_caption">
                                   <p class="h2_small"><strong>Antoine</strong></p>
                                   <p>1ere année medecine</p>
                                   <p>Ma marraine me soutient constamment tout au long de mon année et aujourd’hui mes résultats sont probants. Je me sens en confiance, et je garderai un bon souvenir dans ma Prépa.</p>
-                              </div>
-                            </div>
+                              </div><!-- /temoignage_slider_caption -->
+                            </div><!-- /col-sm-5 -->
                         </div>
                         <div class="item">
                             <div class="col-sm-6 col-sm-offset-1">
                                 <div class="embed-responsive embed-responsive-16by9 col-sm-9">
                                     <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/-fmpxlpgQyM"></iframe>
                                 </div>
-                            </div>
+                            </div><!-- /col-sm-6 -->
                             <div class="col-sm-5 temoignage_back">
-                                <div class="temoignage_slider-caption">
+                                <div class="temoignage_slider_caption">
                                     <p class="h2_small"><strong>Antoine</strong></p>
                                     <p>1ere année medecine</p>
                                     <p>Ma marraine me soutient constamment tout au long de mon année et aujourd’hui mes résultats sont probants. Je me sens en confiance, et je garderai un bon souvenir dans ma Prépa.</p>
-                                </div>
-                            </div>
+                                </div><!-- /temoignage_slider_caption -->
+                            </div><!-- /col-sm-5 -->
                         </div>
                         <div class="item">
                             <div class="col-sm-6 col-sm-offset-1">
                                 <div class="embed-responsive embed-responsive-16by9 col-sm-9">
                                     <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/2cKztlWH_wE"></iframe>
                                 </div>
-                            </div>
+                            </div><!-- /col-sm-6 -->
                             <div class="col-sm-5 temoignage_back">
-                                <div class="temoignage_slider-caption">
+                                <div class="temoignage_slider_caption">
                                     <p class="h2_small"><strong>Antoine</strong></p>
                                     <p>1ere année medecine</p>
                                     <p>Ma marraine me soutient constamment tout au long de mon année et aujourd’hui mes résultats sont probants. Je me sens en confiance, et je garderai un bon souvenir dans ma Prépa.</p>
-                                </div>
-                            </div>
+                                </div><!-- /temoignage_slider_caption -->
+                            </div><!-- /col-sm-5 -->
                         </div>
                     </div>
                     <div class="controllers col-sm-6 col-xs-12">
@@ -122,7 +121,7 @@ Récuperation du header
                         <a class="right temoignage_slider-control" href="#carousel-example-generic" data-slide="next">
                             <span class="glyphicon glyphicon-chevron-right"></span>
                         </a>
-                    </div>
+                    </div><!-- /col-sm-6 -->
                     <div class="controllers col-sm-6 col-xs-12">
                         <!-- Indicators -->
                         <ol class="temoignage_slider-indicators">
@@ -130,11 +129,11 @@ Récuperation du header
                             <li data-target="#temoignage_slider-example-generic" data-slide-to="1"></li>
                             <li data-target="#temoignage_slider-example-generic" data-slide-to="2"></li>
                         </ol>
-                    </div>
-                </div>
+                    </div><!-- /temoignage_slider_caption -->
+                </div><!-- /temoignage_slider -->
             </div><!-- /row -->
         </div><!-- /container -->
-    </section>
+    </section><!-- /temoignages -->
 
     <!--
     Section présentant les chiffres de l'école
@@ -148,23 +147,23 @@ Récuperation du header
                         <div class="cercle-4">
                             <strong class="counter">98</strong><span>%</span>
                             <p>d'étudiants satisfaits de nos formations</p>
-                        </div>
+                        </div><!-- /cercle-4 -->
                     </div><!-- /col-sm-4 -->
                     <div class="col-sm-4">
                         <div class="cercle-4">
                             <strong class="counter">66</strong><span>%</span>
                             <p>de taux de réussite au concours</p>
-                        </div>
+                        </div><!-- /cercle-4 --><!-- /cercle-4 -->
                     </div><!-- /col-sm-4 -->
                     <div class="col-sm-4">
                         <div class="cercle-4">
                             <strong class="counter">900</strong>
                             <p>étudiants préparés à ce concours</p>
-                        </div>
+                        </div><!-- /cercle-4 -->
                     </div><!-- /col-sm-4 -->
                 </div><!-- /row -->
         </div><!-- /container -->
-    </section>
+    </section><!-- /statistiques -->
 
     <!--
     Section présentant les formuless de l'école
@@ -184,11 +183,11 @@ Récuperation du header
                             <p class="button active"><em>En savoir <span>+</span></em></p>
                             <div class="info_sup">
                                 <p>Voila des infos supplémentaires</p>
-                            </div>
-                        </div>
+                            </div><!-- /info_sup -->
+                        </div><!-- /info -->
                         <button class="btn btn-primary center-block" data-toggle="modal" data-target="#signin_form">Inscrivez-vous</button>
                     </div>
-                </div>
+                </div><!-- /col-sm-6 -->
                 <div class="col-sm-6 formules_container">
                     <div>
                         <h2 class="h2_medium">Pack complet</h2>
@@ -201,14 +200,14 @@ Récuperation du header
                             <p class="button active"><em>En savoir <span>+</span></em></p>
                             <div class="info_sup">
                                 <p>Voila des infos supplémentaires</p>
-                            </div>
-                        </div>
+                            </div><!-- /info_sup -->
+                        </div><!-- /info -->
                         <button class="btn btn-primary center-block" data-toggle="modal" data-target="#form">Inscrivez-vous</button>
                     </div>
                 </div><!-- /col-sm-6 -->
             </div><!-- /row -->
         </div><!-- /container -->
-    </section>
+    </section><!-- /formules -->
 
     <?php endwhile;else : ?>
         <p>desolé pas de centres pour l'instant...</p>
